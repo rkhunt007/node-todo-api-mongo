5b9f5373cb32d5f65857f0937b2b81774a5fe8de
Rahul Khunt <rahul.khunt@einfochips.com>
2017-07-14 18:43:07 +0530

    Create custom model method
    Creting UserSchema
    generate and send back jwt token in header
    send only email and id in response body

--- server/models/user.js
+++ server/models/user.js
@@ -4,11 +4,15 @@ const mongoose = require('mongoose');
 	Ref: https://www.npmjs.com/package/validator
 */
 const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
 /*
 	For more Mongoose validation go to http://mongoosejs.com/docs/validation.html
 */
-var User = mongoose.model('User', {
+
+// To create custom model methods
+var UserSchema = new mongoose.Schema({
 	email: {
 		type: String,
 		required: true,
@@ -36,6 +40,27 @@ var User = mongoose.model('User', {
 		}
 	}]
 });
+
+UserSchema.methods.toJSON = function () {
+	var user = this;
+	var userObject = user.toObject();
+
+	return _.pick(userObject, ['_id', 'email']);
+};
+
+UserSchema.methods.generateAuthToken = function() {
+	var user = this;
+	var access = 'auth';
+	var token = jwt.sign({_id: user._id.toHexString(), access}, 'secret').toString();
+	
+	user.tokens.push({access, token});
+
+	return user.save().then(() => {
+		return token;
+	})
+};
+
+var User = mongoose.model('User', UserSchema);
 
 module.exports = {
 	User
