0d1300192e84a56adcdbe1ab822041e77256b801
Rahul Khunt <rahul.khunt@einfochips.com>
2017-07-20 00:03:39 +0530

    added middleware authentication and private route

--- server/models/user.js
+++ server/models/user.js
@@ -60,6 +60,22 @@ UserSchema.methods.generateAuthToken = function() {
 	})
 };
 
+// This is a model method
+UserSchema.statics.findByToken = function(token) {
+	var User = this;
+	var decoded;
+	try {
+		decoded = jwt.verify(token, 'secret');
+	} catch(e) {
+		return Promise.reject();
+	}
+	return User.findOne({
+		_id: decoded._id,
+		'tokens.token': token,
+		'tokens.access': 'auth' 
+	});
+}
+
 var User = mongoose.model('User', UserSchema);
 
 module.exports = {
--- server/server.js
+++ server/server.js
@@ -15,6 +15,7 @@ const _ = require('lodash');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 const {ObjectID} = require('mongodb');
 
@@ -118,6 +119,10 @@ app.post('/users', (req, res) => {
 	}).catch((e) => {
 		res.status(400).send(e);
 	});
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+	res.send(res.user);
 });
 
 app.listen(port, () => {
