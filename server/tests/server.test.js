cbcd5132a7529fab23bd329f6f5a0a2235f217dc
Rahul Khunt <rahul.khunt@einfochips.com>
2017-07-21 18:03:09 +0530

    Adding tests for login request

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -169,7 +169,7 @@ describe('POST /users', () => {
 					expect(user).toExist();
 					expect(user.password).toNotBe(password);
 					done();
-				});
+				}).catch((e) => done(e));
 			});
 	});
 
@@ -189,6 +189,58 @@ describe('POST /users', () => {
 			.post('/users')
 			.expect(400)
 			.end(done);
+	});
+
+});
+
+describe('POST /users/login', () => {
+	it('shoud login a user and return valid auth token', (done) => {
+		request(app)
+			.post('/users/login')
+			.send({
+				email: users[1].email,
+				password: users[1].password
+			})
+			.expect(200)
+			.expect((res) => {
+				expect(res.headers['x-auth']).toExist();
+			})
+			.end((err, res) => {
+				if (err) {
+					return done(err);
+				}
+				User.findById(users[1]._id).then((user) => {
+					expect(user.tokens[0]).toInclude({
+						access: 'auth',
+						token: res.headers['x-auth']
+					});
+					done();
+				}).catch((e) => done(e));
+			});
+	});
+
+	it('should reject invalid login', (done) => {
+		// var email = users[0].email;
+		// var password = "123abcd";
+		request(app)
+			.post('/users/login')
+			.send({
+				email: users[1].email,
+				password: 'test'
+			})
+			.expect(400)
+			.expect((res) => {
+				expect(res.headers['x-auth']).toNotExist();
+			})
+			.end((err, res) => {
+				if (err) {
+					return done(err);
+				}
+				User.findById(users[1]._id).then((user) => {
+					expect(user.tokens.length).toBe(0);
+					done();
+				}).catch((e) => done(e));
+			});
 	});
 
 });
