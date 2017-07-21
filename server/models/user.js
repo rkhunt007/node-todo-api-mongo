a3327dc10aa07c388b4c6cfb5a8445d288666902
Rahul Khunt <rahul.khunt@einfochips.com>
2017-07-21 17:47:41 +0530

    Added login route

--- server/models/user.js
+++ server/models/user.js
@@ -77,6 +77,25 @@ UserSchema.statics.findByToken = function(token) {
 	});
 }
 
+UserSchema.statics.findByCredentials = function(email, password) {
+	var User = this;
+	return User.findOne({email}).then((user) => {
+		if (!user) {
+			return Promise.reject();
+		}
+		return new Promise((resove, reject) => {
+			bcrypt.compare(password, user.password, (err, res) => {
+				if (res) {
+					resove(user);
+				} else {
+					reject();
+				}
+			});
+		})
+		
+	})
+};
+
 /*
 	Here we are using mongoose middleware to perform some operation
 	before saving the data to database.
