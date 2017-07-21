b95f42704f96e4be64aa6b39c83e3d4aeb210088
Rahul Khunt <rahul.khunt@einfochips.com>
2017-07-21 19:56:50 +0530

    Remove token to perform logging out operation.

--- server/models/user.js
+++ server/models/user.js
@@ -61,6 +61,15 @@ UserSchema.methods.generateAuthToken = function() {
 	})
 };
 
+UserSchema.methods.removeToken = function(token) {
+	var user = this;
+	return user.update({
+		$pull: {
+			tokens: {token}
+		}
+	});
+};
+
 // This is a model method
 UserSchema.statics.findByToken = function(token) {
 	var User = this;
